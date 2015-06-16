144273fe311c042611f4250d19bcf4d560af1166
Jason Ohr <jason.ohr@gmail.com>
2015-06-16 13:10:09 -0400

    added the functionality needed to remove a post
    1. constructed the deleteBlog function AllBlogs to delete the selected post
    2. constructed the fetchRender function on AllBlogs to fetch the collection and re-render.
    
    todo:
    1. Editing a post
    2. Handle long posts
    3. Add delete/edit in SingleBlog
    4. Add basic validation to CreateBlog

--- scripts/e/router.js
+++ scripts/e/router.js
@@ -1,7 +1,7 @@
 /**
  * Created by firewaterjoe on 6/15/15.
  */
-import {AllBlogs} from './view/eView';
+import {AllBlogs,SingleBlog,CreateBlog} from './view/eView';
 import {BlogCollection} from './models/eModels';
 
 var Router = Backbone.Router.extend({
@@ -13,15 +13,36 @@ var Router = Backbone.Router.extend({
     },
     initialize: function(){
         this.blogCollection = new BlogCollection();
+        //this.blogCollectionPromise = this.blogCollection.fetch();
+        this.blogCollection.fetch();
 
     },
     blogIndex: function(){
-        $('#app').html(new AllBlogs({model: this.blogCollection}).el);
-    },
-    newBlog: function(){
+        //this.blogCollectionPromise.then(function(){
+        //    console.log('collection',this.blogCollectionPromise);
+        //    $('#app').html(new AllBlogs({collection: this.blogCollectionPromise}).el);
+        //
+        //}.bind(this))
+        this.blogCollection.fetch().then(function(){
+            console.log('collection',this.blogCollection);
+            $('#app').html(new AllBlogs({collection: this.blogCollection}).el);
+        }.bind(this));
 
     },
-    blogShow: function(){
+    newBlog: function(){
+        this.blogCollection.fetch().then(function(){
+            $('#app').html(new CreateBlog({collection: this.blogCollection}).el);
+
+        }.bind(this));
+    },
+    blogShow: function(id){
+
+        this.blogCollection.fetch().then(function(){
+            this.model = this.blogCollection.get(id);
+            $('#app').html(new SingleBlog({model:this.model}).el);
+        }.bind(this));
+
+
 
     },
     editBlog: function(){
--- scripts/e/view/eView.js
+++ scripts/e/view/eView.js
@@ -5,30 +5,74 @@
 var AllBlogs = Backbone.View.extend ({
     template: JST.the_blogs,
     events : {
-        'click .blog-single': 'blogSelected'
+        'click .delete-blog': 'deleteBlog'
     },
     initialize: function(){
-        this.render()
+
+        this.listenTo(this.collection, 'update', this.fetchRender );
+        this.render();
     },
     render: function(){
-        this.$el.html(this.template())
+
+        this.$el.html(this.template(this.collection.toJSON()))
     },
     blogSelected: function(e){
         e.preventDefault();
         var blog = $('.blog-single');
         var dude = $(e.target).closest(blog).attr('id');
+    },
+    deleteBlog: function(e){
+        e.preventDefault();
+        var blog = $('.blog-single');
+        this.model = this.collection.get($(e.target).closest(blog).attr('id'));
+        this.model.destroy();
 
-
-
+        //console.log(this.model);
+    },
+    fetchRender: function(){
+        this.collection.fetch().then(function(){
+            this.render();
+        }.bind(this));
     }
 });
 
 var SingleBlog = Backbone.View.extend ({
-
+    template: JST.single_blog,
+    initialize: function(){
+        this.render()
+    },
+    render: function(){
+        console.log(this.model);
+        this.$el.html(this.template(this.model.toJSON()));
+    }
 });
 
 var CreateBlog = Backbone.View.extend({
+    template: JST.blog_form,
+    events:{
+      'submit': 'postBlog'
+    },
 
+    initialize: function(){
+
+        this.render();
+    },
+    render: function(){
+        this.$el.html(this.template())
+    },
+    postBlog: function(e){
+        e.preventDefault();
+        console.log('what up');
+        this.collection.create({
+            title: $('.title').val(),
+            content: $('.content').val()
+        });
+        this.clearContent();
+    },
+    clearContent: function(){
+        $('.title').val('');
+        $('.content').val('');
+    }
 });
 var EditBlog = Backbone.View.extend({
 
